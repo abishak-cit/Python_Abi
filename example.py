4ce9229b82c4706406948be1fd33910fcd50be63
abishak-cit <54836984+abishak-cit@users.noreply.github.com>
2019-09-03 14:12:25 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,11 @@
+from math import pow
+number = int(input("Enter the number:"))
+sum=0
+while(number>0):
+    number1 = number % 10
+    sum = sum + pow(number1,2)
+    number = number//10
+print(sum)
+
+
+ 
