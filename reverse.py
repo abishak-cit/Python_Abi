f0226ada1cf18757c30dee7ebf5f6cb34cbf6097
abishak-cit <54836984+abishak-cit@users.noreply.github.com>
2019-09-05 20:43:37 +0530

    Add files via upload

--- reverse.py
+++ reverse.py
@@ -1,8 +1,14 @@
-def reverse(S):
-  input_string = S.split(" ")
-  input_string = input_string[-1::-1]
-  output = ' '.join(input_string)
-  return output
-if __name__ =='__main__':
-  S='hello world'
-  print (reverse(S))
+#input of num
+num = int(input())
+#initial value of reverse
+reverse_num = 0
+#conditon for entry using while loop
+while(num>0):
+  #remainder of the given no
+  remainder = num % 10
+  # formula of the reverse
+  reverse_num = (reverse_num * 10) + remainder
+  #floor division of the num
+  num = num//10
+#display the reverse
+print(reverse_num)
