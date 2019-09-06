d60fe9e79b2c99ab40feba18794b53a93ebdb37a
abishak-cit <54836984+abishak-cit@users.noreply.github.com>
2019-09-06 15:46:50 +0530

    Add files via upload

--- perfect.py
+++ perfect.py
@@ -1,11 +1,10 @@
 import math
-def isperfect(product):
-    scr = math.sqrt(product)
-    return ((scr-math.floor(scr))== 0)
-N = int(input())
-M =int(input())
-product = N*M
-if(isperfect(product)):
+def isperfectsquare(x,y):
+    z = x * y
+    s = math.sqrt(z)
+    return (s - math.floor(s) == 0)
+x,y = map(int,(input().split()))
+if(isperfectsquare(x,y)):
     print('yes')
 else:
     print('no')
